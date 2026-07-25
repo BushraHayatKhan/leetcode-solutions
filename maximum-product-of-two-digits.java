daa76a9341edf9af8d972dd31d62ddb7119e6d48
Bushra Hayat Khan <khanhayatbushra@gmail.com>
2026-07-25 23:50:26 +0530

    Maximum Product of Two Digits Solution

---
+++ maximum-product-of-two-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int n) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        while (n != 0) {
+            int d = n % 10;
+            if (d > max1) {
+                max2 = max1;
+                max1 = d;
+            } else if (d > max2) {
+                max2 = d;
+            }
+            n = n / 10;
+        }
+        return max1 * max2;
+    }
+}
